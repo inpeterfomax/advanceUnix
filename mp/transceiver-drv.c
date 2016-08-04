e30a5174c39c404617a96abb6f566f5898ffd116
peter <peter.gu>
2016-08-04 20:49:36 +0800

    08-04: 08-04: 08-04: 08-04: 08-04: 08-04: 08-04: 08-04: basic char driver and insmod ; need to enable gpio interrupt function;

--- mp/transceiver-drv.c
+++ mp/transceiver-drv.c
@@ -1,32 +1,28 @@
-
+/*
+* name: transceiver-drv.c.
+* author:peter_gu@realsil.com.cn.
+* function:cmd interrupt handler and response sender.
+* version:v1.0
+*/
 
 #include "transceiver-drv.h"
 
-#include <linux/module.h>
-#include <linux/init.h>
-#include <linux/kernel.h>
-#include <linux/fs.h>
-#include <linux/types.h>
-#include <kdev_t.h>
-#include <linux/cdev.h>
-#include <linux/device.h>
-
-#include <net/netlink.h>
-
 #define SUCCESS 0
-#define DEVICE_NAME "chardev" /* Dev name as it appears in /proc/devices   */
+#define DEVICE_NAME "transceiver" /* Dev name as it appears in /proc/devices   */
+#define CLASS_NAME "transceiver-class"
 #define BUF_LEN 80            /* Max length of the message from the device */
 
 //#define IRQNUM gpio_irq
 
-static int major,minor;            /* Major,Minor number assigned to our device driver */
+static int major,minor;      /* Major,Minor number assigned to our device driver */
 static int device_open = 0;  /* Is device open?  Used to prevent multiple
                                         access to the device */
-static cdev_t mycdev=0;		/* Transciver dev number,major & minor*/
+static unsigned int mycdevnum = 0;	 /* Transciver dev number,major & minor*/
 static char msg[BUF_LEN];    /* The msg the device will give when asked    */
 static char *msg_Ptr;
 
-#define CLASS_NAME "chardev-class"
+struct local_dev *mydev=NULL;
+
 
 static struct class *tranceiver_class;
 static struct device *tranceiver_dev; 
@@ -36,23 +32,26 @@ static int local_open(struct inode *node,struct file*filp)
 	return 0;
 }
 
-static void local_release(struct inode *node,struct file*filp)
+static int local_release(struct inode *node,struct file*filp)
 {
+	return 0;
 }
 
-static void local_ioctl(struct inode *node,struct file *filp,
+static int local_ioctl(struct inode *node,struct file *filp,
 		 unsigned int ioctl_num,unsigned long ioctl_param)
 {
+	return 0;
 }
 
-static void local_mmap(struct file *filp, struct vm_area_struct *vma){
-
+static int local_mmap(struct file *filp, struct vm_area_struct *vma)
+{
+	return 0;
 }
 
 static const struct file_operations transceiver_ops={
 	.owner = THIS_MODULE,
 	.open = local_open,
-	.close = local_release,
+	.release = local_release,
 	.ioctl = local_ioctl,
 	.mmap = local_mmap,
 };
@@ -83,27 +82,50 @@ static struct device_attribute device_attrs[]={
 	NULL,
 };
 
+irq_handler cmd_handler(){
+	clean_irq_state(s);
+	disable_irq();
+
+	/*send cmd msg to mpv50 */
+	cmdmsg = alloc_msg;
+	send_cmd_to_mpv50;
+	sendmsg(cmdmsg);
+
+	/*wait for response msg from mpv50*/
+	write_resp_to_register();
+}
+
 static __init int transceiver_init(void)
 {
 	int retval,i;
-	ret = alloc_chrdev_region(&mycdev,0,1,DEVICE_NAME);
+	mydev =(struct local_dev *) kmalloc(sizeof(struct local_dev),GFP_KERNEL);
+	if(!mydev){
+		printk("%s %d kmalloc fail\n",__FUNCTION__,__LINE__);
+		return -1;
+	}
+	retval = alloc_chrdev_region(&mycdevnum,0,1,DEVICE_NAME);
 	if(retval < 0){
 		printk("%s %d , alloc fail\n",__FUNCTION__,__LINE__);
 		goto alloc_fail;
 	}
-	major = MAJOR(mycdev);
-	retval = register_chrdev(major,DEVICE_NAME,&transceiver_ops);/* little old one.*/ 
-	if(retval < 0){
-		printk("%s %d , register fail\n",__FUNCTION__,__LINE__);
-		goto register_fail;
+	major = MAJOR(mycdevnum);
+
+	cdev_init(&mydev->cdev,&transceiver_ops);
+	mydev->cdev.owner = THIS_MODULE;
+	mydev->cdev.ops = &transceiver_ops;
+	retval = cdev_add(&mydev->cdev,mycdevnum,1);
+	if( retval ){
+		printk("%s %d , retval=%d \n",__FUNCTION__,__LINE__,retval);
+		goto class_fail;
 	}
-	tranceiver_class = creat_class(THIS_MODULE,CLASS_NAME);
+	
+	tranceiver_class = class_create(THIS_MODULE,CLASS_NAME);
 	if( IS_ERR(tranceiver_class) ){
 		retval =  PTR_ERR(tranceiver_class);
 		goto class_fail;
 	}
 	/* With a class, the easiest way to instantiate a device is to call device_create() . Creat /dev/device */
- 	tranceiver_dev = device_create(tranceiver_class, NULL, mycdev, NULL, CLASS_NAME "_" DEVICE_NAME);
+ 	tranceiver_dev = device_create(tranceiver_class, NULL, mycdevnum, NULL, CLASS_NAME "_" DEVICE_NAME);
 	if (IS_ERR(tranceiver_dev)) {
 		printk("failed to create device '%s_%s'\n", CLASS_NAME, DEVICE_NAME);
 	    retval = PTR_ERR(tranceiver_dev);
@@ -119,32 +141,40 @@ static __init int transceiver_init(void)
 		while (--i >= 0)
 			device_remove_file(tranceiver_dev, &device_attrs[i]);
 	}
+
+	/*alloc irq for cmd done*/
+	retval = request_irq(gpio_to_irq(gpio),handler,flag,device);
+
+	
 	return 0;
 	
 failed_devreg:
 	class_destroy(tranceiver_class);
 	return retval;
 class_fail:
-	unregister_chrdev(major,DEVICE_NAME);
-	unregister_chrdev_region(mycdev,DEVICE_NAME);
-
+	unregister_chrdev_region(mycdevnum,DEVICE_NAME);
+	return -1;
 register_fail:
-	return ret;
+	return retval;
 
 alloc_fail:
-	return ret;
+	return retval;
 	
 }
 static __exit void transceiver_clean(void)
 {
 	int i;
+		
 	/*Remove attribute files*/
 	for (i = 0; i < ARRAY_SIZE(device_attrs); i++) {
 		device_remove_file(tranceiver_dev, &device_attrs[i]);
 	}
-	device_destroy(tranceiver_class, mycdev);
+	device_destroy(tranceiver_class, mycdevnum);
 	class_destroy(tranceiver_class);
 	unregister_chrdev(major, DEVICE_NAME);
+	/*free mydev struct.*/
+	if( mydev )
+		kfree(mydev);
 }
 
 
@@ -152,10 +182,10 @@ static __exit void transceiver_clean(void)
 module_init(transceiver_init);
 module_exit(transceiver_clean);
 
-#define DESCRIPTION "create an vitrual cdev,
-					it takes responsibility to handle gpio interrupt
-					then send cmd to mpv50 process
-					and receive msg form mpv50 then write back response 
+#define DESCRIPTION "create an vitrual cdev, \
+					it takes responsibility to handle gpio interrupt \
+					then send cmd to mpv50 process \
+					and receive msg form mpv50 then write back response \
 					to resp-register."
 
 MODULE_LICENSE("GPL");
--- mp/transceiver-drv.h
+++ mp/transceiver-drv.h
@@ -1,15 +1,28 @@
 #ifndef _TRANSCEIVER_H_
 #define _TRANSCEIVER_H_
 
-struct local_ts_dev{
-	cdev dev;
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/types.h>
+#include <linux/kdev_t.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+
+#include <net/netlink.h>
+
+#include <linux/slab.h>	//for kmalloc
+
+struct local_dev{
+	struct cdev cdev;
 };
 
 static int local_open(struct inode *, struct file *);
-static void local_release(struct inode *, struct file *);
-static void local_ioctl(struct inode *,struct file *,
+static int local_release(struct inode *, struct file *);
+static int local_ioctl(struct inode *,struct file *,
 		 unsigned int ,unsigned long );
-static void local_mmap(struct file *,struct vm_area_struct *);
+static int local_mmap(struct file *,struct vm_area_struct *);
 
 
 #endif
