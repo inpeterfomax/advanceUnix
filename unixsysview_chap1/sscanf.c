b40b5ef89443e36619dd8911c236ee31c1ea2d7c
peter <peter.gu>
2016-11-07 09:51:20 +0800

    sscanf funciton

--- unixsysview_chap1/sscanf.c
+++ unixsysview_chap1/sscanf.c
@@ -3,12 +3,15 @@
 
 int main()
 {
-	char tmparry[1024]="Hello World!";
+	char tmparry[1024]="Hello World  123 !";
 	char str1[128] = {0};
 	char str2[128] = {0};
-
-	sscanf(tmparry,"%s %s ",str1,str2);
-	printf("source=%s , str1=%s ,str2=%s\n",tmparry,str1,str2);
+	int ai=0;
+//sscanf(tmparry," %s %s %d",str1,str2,&ai);
+	sscanf(tmparry,"Hello World %d !",&ai);
+//	printf("source=%s , str1=%s ,str2=%s ai=%d \n",tmparry,str1,str2,ai);
+//	printf("source=%s , str1=%s ,str2=%s ai=%d \n",tmparry,str1,str2,ai);
+	printf("source=%s , ai=%d \n",tmparry,ai);
 	
 	return 0;
 }
